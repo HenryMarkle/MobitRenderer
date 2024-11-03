930757fe4e29435f44a5c67163196a9fbf5ecb17
Henry Markle <mohammad96alsalloum@outlook.sa>
2024-11-03 18:43:52 +0300

    Working raylib library

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
-#include <iostream>
+#include "raylib.h"
 
-int main(int, char**){
-    std::cout << "Hello, from MobitRenderer!\n";
+int main() {
+    SetTargetFPS(15);
+
+    InitWindow(1200, 800, "Renderer");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(DARKGRAY);
+
+        DrawText("Hello", 0, 0, 30, WHITE);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
