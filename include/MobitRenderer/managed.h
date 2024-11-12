2f5560b27fe0ac2bb6a74182ebfa17b45e4f6b4d
Henry Markle <mohammad96alsalloum@outlook.sa>
2024-11-12 10:30:52 +0300

    Managed raylib resources

---
+++ include/MobitRenderer/managed.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+#include <raylib.h>
+
+namespace mr {
+
+class rendertexture {
+private:
+  RenderTexture2D rt_;
+  bool isloaded;
+
+public:
+  /// @brief Returns a temprary, read-only rederence to the rendertexture.
+  /// @warning Do not copy the returned reference.
+  const RenderTexture2D &get() const noexcept;
+  bool is_loaded() const noexcept;
+  void unload();
+  RenderTexture2D release();
+
+  rendertexture &operator=(rendertexture &&) noexcept;
+  rendertexture &operator=(const rendertexture &) = delete;
+
+  rendertexture() = delete;
+  rendertexture(const rendertexture &) = delete;
+  rendertexture(rendertexture &&) noexcept;
+  explicit rendertexture(uint16_t width, uint16_t height);
+
+  ~rendertexture();
+};
+
+class image {
+private:
+  Image img_;
+  bool isloaded;
+
+public:
+  /// @brief Returns a temprary, read-only rederence to the image.
+  /// @warning Do not copy the returned reference.
+  const Image &get() const noexcept;
+  bool is_loaded() const noexcept;
+  void unload();
+  Image release() noexcept;
+
+  image &operator=(image &&) noexcept;
+  image &operator=(const image &) = delete;
+
+  image() = delete;
+  image(const image &) = delete;
+  image(image &&) noexcept;
+  explicit image(const std::string &);
+  explicit image(const char *);
+
+  ~image();
+};
+
+class texture {
+private:
+  Texture2D texture_;
+  bool isloaded;
+
+public:
+  /// @brief Returns a temprary, read-only rederence to the texture.
+  /// @warning Do not copy the returned reference.
+  const Texture2D &get() const noexcept;
+  bool is_loaded() const noexcept;
+  void unload();
+
+  /// @brief Releases ownership of the texture and returns it.
+  Texture2D release() noexcept;
+
+  texture &operator=(texture &&) noexcept;
+  texture &operator=(const texture &) = delete;
+
+  texture() = delete;
+  texture(const texture &) = delete;
+  texture(texture &&) noexcept;
+
+  explicit texture(const std::string &);
+  explicit texture(const char *);
+  explicit texture(const Image *);
+  explicit texture(const Image &);
+  explicit texture(const image &);
+
+  ~texture();
+};
+
+typedef texture unique_texture;
+typedef rendertexture unique_rendertexture;
+typedef image unique_image;
+
+}; // namespace mr
---
+++ src/managed.cpp
@@ -0,0 +1,163 @@
+#include <cstdint>
+#include <string>
+
+#include <raylib.h>
+
+#include <MobitRenderer/managed.h>
+
+namespace mr {
+
+const RenderTexture2D &rendertexture::get() const noexcept { return rt_; }
+bool rendertexture::is_loaded() const noexcept { return isloaded; }
+void rendertexture::unload() {
+  UnloadRenderTexture(rt_);
+  isloaded = false;
+}
+RenderTexture2D rendertexture::release() {
+  isloaded = false;
+
+  auto rt = rt_;
+  rt_ = RenderTexture2D{.id = 0, .texture = {.id = 0}};
+  return rt;
+}
+rendertexture &rendertexture::operator=(rendertexture &&other) noexcept {
+  if (this == &other)
+    return *this;
+
+  unload();
+
+  rt_ = other.rt_;
+  isloaded = other.isloaded;
+
+  other.rt_ = RenderTexture2D{.id = 0};
+  other.isloaded = false;
+
+  return *this;
+}
+rendertexture::rendertexture(rendertexture &&other) noexcept {
+  rt_ = other.rt_;
+  isloaded = other.isloaded;
+
+  other.rt_ = RenderTexture2D{.id = 0};
+  other.isloaded = false;
+}
+rendertexture::rendertexture(uint16_t width, uint16_t height) {
+  rt_ = LoadRenderTexture(width, height);
+  isloaded = true;
+}
+rendertexture::~rendertexture() { unload(); }
+
+const Image &image::get() const noexcept { return img_; }
+bool image::is_loaded() const noexcept { return isloaded; }
+void image::unload() {
+  if (!isloaded)
+    return;
+  UnloadImage(img_);
+  isloaded = false;
+}
+Image image::release() noexcept {
+  auto img = img_;
+  img_ = Image{.data = nullptr, .width = 0, .height = 0};
+
+  isloaded = false;
+  return img;
+}
+image &image::operator=(image &&other) noexcept {
+  if (this == &other)
+    return *this;
+
+  unload();
+
+  img_ = other.img_;
+  isloaded = other.isloaded;
+
+  other.img_ = Image{
+      .data = nullptr,
+      .width = 0,
+      .height = 0,
+  };
+  other.isloaded = false;
+
+  return *this;
+}
+image::image(image &&other) noexcept {
+  img_ = other.img_;
+  isloaded = other.isloaded;
+
+  other.img_ = Image{
+      .data = nullptr,
+      .width = 0,
+      .height = 0,
+  };
+  other.isloaded = false;
+}
+image::image(const std::string &path) {
+  img_ = LoadImage(path.c_str());
+  isloaded = true;
+}
+image::image(const char *path) {
+  img_ = LoadImage(path);
+  isloaded = true;
+}
+image::~image() { unload(); }
+
+const Texture2D &texture::get() const noexcept { return texture_; }
+bool texture::is_loaded() const noexcept { return isloaded; }
+void texture::unload() {
+  if (!isloaded)
+    return;
+
+  isloaded = false;
+  UnloadTexture(texture_);
+}
+Texture2D texture::release() noexcept {
+  auto texture = texture_;
+  texture_ = Texture2D{.id = 0};
+
+  isloaded = false;
+  return texture;
+}
+texture &texture::operator=(texture &&other) noexcept {
+  if (this == &other)
+    return *this;
+
+  unload();
+
+  texture_ = other.texture_;
+  isloaded = other.isloaded;
+
+  other.texture_ = Texture2D{.id = 0};
+  other.isloaded = false;
+
+  return *this;
+}
+texture::texture(texture &&other) noexcept {
+  texture_ = other.texture_;
+  isloaded = other.isloaded;
+
+  other.texture_ = Texture2D{.id = 0};
+  other.isloaded = false;
+}
+texture::texture(const std::string &path) {
+  texture_ = LoadTexture(path.c_str());
+  isloaded = true;
+}
+texture::texture(const char *path) {
+  texture_ = LoadTexture(path);
+  isloaded = true;
+}
+texture::texture(const Image *image) {
+  texture_ = LoadTextureFromImage(*image);
+  isloaded = true;
+}
+texture::texture(const Image &image) {
+  texture_ = LoadTextureFromImage(image);
+  isloaded = true;
+}
+texture::texture(const image &image) {
+  texture_ = LoadTextureFromImage(image.get());
+  isloaded = true;
+}
+texture::~texture() { unload(); }
+
+}; // namespace mr
